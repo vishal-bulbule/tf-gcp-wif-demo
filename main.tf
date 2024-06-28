46ce781abed5c0d613c678274771c1283ded6931
Vishal Bulbule <143475073+vishal-bulbule@users.noreply.github.com>
2024-06-28 17:08:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "google_storage_bucket" "my-bucket" {
   force_destroy = true
   public_access_prevention = "enforced"
 }
+
+resource "google_storage_bucket" "my-bucket2" {
+  name          = "bkt-demo-002"
+  location      = "us-central1"
+  project = "tt-dev-001"
+  force_destroy = true
+  public_access_prevention = "enforced"
+}
